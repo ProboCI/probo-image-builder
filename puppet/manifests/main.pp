7eebc780adc709f47f7c6dcd55e5e372c04f9736
Howard Tyson <howard@howardtyson.com>
2014-05-29 01:04:40 -0400

    Fleshing out main puppet manifest with docker and packer installation as well as node.js setup.

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -1,10 +1,27 @@
+
+class { 'docker':
+  manage_kernel => false,
+}
+
+class { 'packer':
+  install_dir => '/opt/packer',
+  version => '0.6.0',
+}->
+
+file { '/usr/bin/packer':
+  ensure => 'link',
+  target => '/opt/packer/packer',
+}
+
 package { 'zip':
   ensure => 'installed',
 }->
 
-class { 'packer':
-  install_dir => '/opt/packer',
-  version => '0.5.2',
-}
+class { 'nodejs':
+  manage_repo => true,
+}~>
 
-include nodejs
+file { '/usr/bin/node':
+  ensure => 'link',
+  target => '/usr/bin/nodejs',
+}
