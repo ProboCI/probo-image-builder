4d84821b225c3ae7afa1c55a8b3238d2fcf3729c
Howard Tyson <howard@howardtyson.com>
2014-05-30 12:58:31 -0400

    Prevent warning resulting from missing hierra file.

--- builder/install_puppet.sh
+++ builder/install_puppet.sh
@@ -1,7 +1,7 @@
 apt-get install -y puppet vim
-# touch /etc/puppet/hiera.yaml
-# # apt-get install -y wget
-# # wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
-# # dpkg -i puppetlabs-release-precise.deb
-# # apt-get update
-# # apt-get install -y puppet
+touch /etc/puppet/hiera.yaml
+
+# Should we use the official puppetlabs repo?
+# apt-get install -y wget
+# wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+# dpkg -i puppetlabs-release-precise.deb
