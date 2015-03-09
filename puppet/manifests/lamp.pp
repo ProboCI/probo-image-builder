10ecf10ce0760e45121d9f6f409bab4cecf5ffd8
Howard Tyson <howard@howardtyson.com>
2015-03-09 15:54:05 -0400

    Updating lepew image building to use new published puppet modules.

--- puppet/manifests/lamp.pp
+++ puppet/manifests/lamp.pp
@@ -1,41 +1,45 @@
-package { 'git':
+include git
+include creature_comforts
+#include wget
+
+package { 'wget':
   ensure => 'installed',
+}->
+
+class { drupal_php:
+  server_manage_service => true,
+  server_service_enable => false,
+  server_service_ensure => 'stopped',
 }
 
-include creature_comforts
+include drush_fetcher
 
-class { 'redis':
+drush::config { 'fetcher-class':
+  file  => 'fetcher-services',
+  key   => "fetcher']['info_fetcher.class",
+  value => 'FetcherServices\InfoFetcher\FetcherServices',
+}
+
+drush::config { 'fetcher-services-host':
+  file  => 'fetcher-services',
+  key   => "fetcher']['info_fetcher.config']['host",
+  value => 'https://extranet.zivtech.com',
+}
+
+drush::config { 'fetcher-services-server-port':
+  file   => 'fetcher-services',
+  key    => "fetcher']['server.port",
+  string => false,
+  value  => $drupal_php::server::apache::server_port,
+}
+
+package { 'redis-server':
+  ensure => 'installed',
 }
 
 class { 'memcached':
   max_memory => '10%'
 }
-
-class { 'apache':
-  default_mods   => false,
-  mpm_module     => 'prefork',
-  servername     => 'dev.local',
-  service_enable => false,
-  # Do not purge configs because it will remove any vhosts we put in palce.
-  purge_configs => false,
-}
-
-# TODO: Match this up with acquia's web heads https://gist.github.com/beejeebus/34c4e8fb4f76dd9ec47e
-class { 'apache::mod::mime': }->
-
-class { 'apache::mod::rewrite': }->
-
-class { 'apache::mod::deflate': }->
-
-# TODO: We probably want this module but this isn't working for some reason.
-# class { 'apache::mod::expires': }->
-class { 'apache::mod::ssl': }
-
-# mod php is not included here beacuse it is added by the php::apache class.
-
-Php::Extension <| |> -> Php::Config <| |> ~> Service["apache2"]
-
-include php
 
 # We need to set the root password so that the user can be loaded.
 user { 'root':
@@ -44,69 +48,22 @@ user { 'root':
   password => '$6$Q6Rg6ZS3$i4ibqDLRQQvJHq0OSOWhDGjSGmBdXaTLhDWkWxKgRfcX8Ll5BSFIME67Q4HL/75LwJHcZoCDfFA8LuGlpjyQK0',
 }
 
-# TODO: Add support for yum.
-class { 'php::cli':
-  ensure   => 'present',
-  provider => 'apt',
-}
+# TODO: opcache tuning.
+# class { 'php::extension::opcache':
+#   require => Class['php::dev'],
+#   # settings => [
+#   #   'set ".anon/opcache.max_accelerated_files" "12000"',
+#   #   'set ".anon/opcache.memory_consumption" "256"',
+#   #   'set ".anon/opcache.validate_timestamps" "0"',
+#   # ],
+# }~>
+# file { '/etc/php5/apache2/conf.d/opcache2.ini':
+#   ensure  => file,
+#   content => "opcache.max_accelerated_files = 12000\nopcache.memory_consumption = 256\nopcache.validate_timestamps = 0",
+#   owner   => 'root',
+#   group   => 'root',
+# }
 
-class { 'php::apache':
-  ensure   => 'present',
-  provider => 'apt',
-}
-
-class { 'php::dev':
-  ensure   => 'present',
-  provider => 'apt',
-}
-
-package { 'curl':
-  ensure => 'present',
-}
-
-class { 'php::extension::curl':
-  require => Class['php::dev'],
-}
-
-class { 'php::pear':
-  require => Class['php::dev'],
-}->
-
-
-php::config { 'php-extension-redis':
-  file => "${php::params::config_root_ini}/redis.ini",
-  config => [],
-}
-
-file { '/etc/php5/apache2/redis.ini':
-
-}
-
-package { 'uploadprogress':
-  ensure   => 'installed',
-  provider => 'pecl',
-}->
-
-class { 'php::extension::opcache':
-  require => Class['php::dev'],
-  # settings => [
-  #   'set ".anon/opcache.max_accelerated_files" "12000"',
-  #   'set ".anon/opcache.memory_consumption" "256"',
-  #   'set ".anon/opcache.validate_timestamps" "0"',
-  # ],
-}~>
-file { '/etc/php5/apache2/conf.d/opcache2.ini':
-  ensure  => file,
-  content => "opcache.max_accelerated_files = 12000\nopcache.memory_consumption = 256\nopcache.validate_timestamps = 0",
-  owner   => 'root',
-  group   => 'root',
-}
-
-
-
-class { 'php::extension::gd':
-  require => Class['php::dev'],
-}
 
 package { 'zip':
   ensure   => 'installed',
@@ -122,36 +79,9 @@ class { 'mysql::server':
   restart          => false,
 }
 
-class { 'mysql::bindings':
-  php_enable => true,
-}
-
-class { "ssh::server":
-  permit_root_login       => 'yes',
-  manage_service          => false,
-  password_authentication => 'yes',
-  use_pam                 => 'no',
-  use_dns                 => 'no',
-}~>
-
-file { '/var/run/sshd':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
-
-}
-
-package { 'wget':
-  ensure => 'installed',
+package { 'apt-transport-https':
+  ensure => 'present',
 }->
-
-class { 'php::composer': }
-
-include php::phpunit
-
-class { 'drush_fetcher':
-  fetcher_host => 'https://extranet.zivtech.com'
-}
 
 class { 'nodejs':
   manage_repo => true,
@@ -165,22 +95,4 @@ file { '/usr/bin/node':
 package { 'lepew-penelope':
   ensure   => 'installed',
   provider => 'npm',
-}
-
-package { 'Console_Table':
-  ensure   => installed,
-  provider => pear,
-}->
-
-vcsrepo { '/usr/lib/drush':
-  provider => 'git',
-  source   => 'https://github.com/drush-ops/drush.git',
-  revision => '6.4.0',
-  require        => Package['git'],
-}->
-
-file { '/usr/local/bin/drush':
-  ensure => 'link',
-  target => '/usr/lib/drush/drush',
-  mode   => 755,
 }
