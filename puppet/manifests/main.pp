0e91824294b7b2694d70c3caabb2957734caedaf
Laurence Liss <laurence@zivtech.com>
2016-02-16 14:09:12 -0500

    Fix missing packer directory.
    Packer was failing to properly install because the directory specified
    was missing. This sets that as a task that runs before packer is
    downloaded.

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -3,6 +3,11 @@ class { 'docker':
   manage_kernel => false,
 }
 
+file { '/opt/packer':
+    ensure => 'directory',
+    mode   => '0755'
+}->
+
 class { 'packer':
   bin_dir => '/opt/packer',
   version => '0.8.6',
