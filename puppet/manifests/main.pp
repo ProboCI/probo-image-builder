436dd4b23756779f4be00dbe9313e3e6a895d5e1
Laurence Liss <laurence@zivtech.com>
2017-07-24 13:15:58 -0400

    Switch to ansible provisioning of docker images.

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -17,26 +17,3 @@ file { '/usr/bin/packer':
   ensure => 'link',
   target => '/opt/packer/packer',
 }
-
-package { 'zip':
-  ensure => 'installed',
-}->
-
-class { 'nodejs':
-  version => 'v4.3.0',
-  make_install => false,
-}->
-
-file { '/usr/bin/node':
-  ensure => 'link',
-  target => '/usr/local/node/node-default/bin/node',
-}->
-
-package { 'lepew-penelope':
-  ensure   => 'installed',
-  provider => 'npm'
-}
-
-package { 'git':
-  ensure => 'installed',
-}
--- scripts/puppet_setup.sh
+++ scripts/puppet_setup.sh
@@ -4,8 +4,13 @@ sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 # Don't use the us mirrors, they sometimes cause trouble.
 sed -i 's/\/us\./\//g' /etc/apt/sources.list
 # Update our package list, especially since we changed the sources.
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F438280EF8D349F
 sudo apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 # Install puppet.
-sudo apt-get install puppet -y
+DEBIAN_FRONTEND=noninteractive apt-get install vim puppet -y
 # Create an empty hierra.yaml so puppet doesn't complain when it is not there.
+mkdir -p /etc/puppetlabs/puppet
+mkdir -p /etc/puppet
+echo '' > /etc/puppetlabs/puppet/hiera.yaml
 echo '' > /etc/puppet/hiera.yaml
