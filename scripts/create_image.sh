99d73fc51928d2da2d7f466f7371eb994f296075
Laurence Liss <laurence@zivtech.com>
2017-08-01 13:06:05 -0400

    Update docker image creation scripts.

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-cd /vagrant/builder
+cd /vagrant/builder/14.04-php5.6
 /usr/local/bin/packer build ubuntu-14.04-lamp.json
-docker build -t="proboci/ubuntu-14.04-lamp:nightly" .
+docker build -t="proboci/ubuntu-14.04-lamp:php5.6-nightly" .
+
+cd ../14.04-php7.0
+/usr/local/bin/packer build ubuntu-14.04-lamp.json
+docker build -t="proboci/ubuntu-14.04-lamp:php7.0-nightly" .
+
+cd ../14.04-php7.1
+/usr/local/bin/packer build ubuntu-14.04-lamp.json
+docker build -t="proboci/ubuntu-14.04-lamp:php7.1-nightly" .
