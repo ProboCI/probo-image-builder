c66905412729dbf29d3c1e180dbceaa9b2917076
Laurence Liss <laurence@zivtech.com>
2017-08-15 17:44:19 -0400

    Add 16.04 docker builds.

--- scripts/export_image.sh
+++ scripts/export_image.sh
@@ -4,3 +4,6 @@ rm -f /vagrant/workspace/*
 docker save proboci/ubuntu-14.04-lamp:php5.6-nightly > /vagrant/workspace/ubuntu-14.04-php5.6-nightly.tar
 docker save proboci/ubuntu-14.04-lamp:php7.0-nightly > /vagrant/workspace/ubuntu-14.04-php7.0-nightly.tar
 docker save proboci/ubuntu-14.04-lamp:php7.1-nightly > /vagrant/workspace/ubuntu-14.04-php7.1-nightly.tar
+docker save proboci/ubuntu-16.04-lamp:php7.0-nightly > /vagrant/workspace/ubuntu-16.04-php7.0-nightly.tar
+docker save proboci/ubuntu-16.04-lamp:php7.1-nightly > /vagrant/workspace/ubuntu-16.04-php7.1-nightly.tar
+docker save proboci/ubuntu-16.04-lamp:php7.2-nightly > /vagrant/workspace/ubuntu-16.04-php7.1-nightly.tar
