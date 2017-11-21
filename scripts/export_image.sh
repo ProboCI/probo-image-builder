53cba9096bb3c2a05b64da7bfd3a7517d7d33250
Jason Moore <jason@zivtech.com>
2017-11-21 13:56:51 -0500

    Updating deprecated Ansible include tasks steps.

--- scripts/export_image.sh
+++ scripts/export_image.sh
@@ -6,4 +6,4 @@ docker save proboci/ubuntu-14.04-lamp:php7.0-nightly > /vagrant/workspace/ubuntu
 docker save proboci/ubuntu-14.04-lamp:php7.1-nightly > /vagrant/workspace/ubuntu-14.04-php7.1-nightly.tar
 docker save proboci/ubuntu-16.04-lamp:php7.0-nightly > /vagrant/workspace/ubuntu-16.04-php7.0-nightly.tar
 docker save proboci/ubuntu-16.04-lamp:php7.1-nightly > /vagrant/workspace/ubuntu-16.04-php7.1-nightly.tar
-docker save proboci/ubuntu-16.04-lamp:php7.2-nightly > /vagrant/workspace/ubuntu-16.04-php7.1-nightly.tar
+docker save proboci/ubuntu-16.04-lamp:php7.2-nightly > /vagrant/workspace/ubuntu-16.04-php7.2-nightly.tar
