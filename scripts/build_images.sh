4d5f224cea30fe583dbacecc5134d1fca5b3239e
Jason Moore <jason@zivtech.com>
2017-12-18 16:32:32 -0500

    Cleaning up and getting setup for being based om Matt's PR.

---
+++ scripts/build_images.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+INFO='\033[0;34m'
+NC='\033[0m' # No Color
+
+PACKER=/usr/local/bin/packer
+BUILDER=/vagrant/builder
+
+build_ubuntu-14.04-lamp-php5.6 () {
+	cd $BUILDER/14.04-php5.6
+	$PACKER build ubuntu-14.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php5.6-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-14.04-lamp:php5.6-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php5.6-nightly built.${NC}"
+}
+
+build_ubuntu-14.04-lamp-php7.0 () {
+	cd $BUILDER/14.04-php7.0
+	$PACKER build ubuntu-14.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php7.0-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-14.04-lamp:php7.0-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php7.0-nightly built.${NC}"
+}
+
+build_ubuntu-14.04-lamp-php7.1 () {
+	cd $BUILDER/14.04-php7.1
+	$PACKER build ubuntu-14.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php7.1-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-14.04-lamp:php7.1-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php7.1-nightly built.${NC}"
+}
+
+build_ubuntu-14.04-lamp-php7.2 () {
+	cd $BUILDER/14.04-php7.2
+	$PACKER build ubuntu-14.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php7.2-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-14.04-lamp:php7.2-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php7.2-nightly built.${NC}"
+}
+
+build_ubuntu-16.04-dotnet-sdk1.0.1 () {
+	cd $BUILDER/16.04-dotnetsdk1.0.1
+	$PACKER build ubuntu-16.04-dotnet.json
+	echo -e "${INFO}Building proboci/ubuntu-16.04-dotnet:sdk1.0.1-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-16.04-dotnet:sdk1.0.1-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-16.04-dotnet:sdk1.0.1-nightly built.${NC}"
+}
+
+build_ubuntu-16.04-dotnet-sdk2.0.2 () {
+	cd $BUILDER/16.04-dotnetsdk2.0.2
+	$PACKER build ubuntu-16.04-dotnet.json
+	echo -e "${INFO}Building proboci/ubuntu-16.04-dotnet:sdk2.0.2-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-16.04-dotnet:sdk2.0.2-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-16.04-dotnet:sdk2.0.2-nightly built.${NC}"
+}
+
+build_ubuntu-16.04-lamp-php7.0 () {
+	cd $BUILDER/16.04-php7.0
+	$PACKER build ubuntu-16.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:php7.0-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-16.04-lamp:php7.0-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:php7.0-nightly built.${NC}"
+}
+
+build_ubuntu-16.04-lamp-php7.1 () {
+	cd $BUILDER/16.04-php7.1
+	$PACKER build ubuntu-16.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:php7.1-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-16.04-lamp:php7.1-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:php7.1-nightly built.${NC}"
+}
+
+build_ubuntu-16.04-lamp-php7.2 () {
+	cd $BUILDER/16.04-php7.2
+	$PACKER build ubuntu-16.04-lamp.json
+	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:php7.2-nightly...${NC}"
+	docker build --compress -t="proboci/ubuntu-16.04-lamp:php7.2-nightly" .
+	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:php7.2-nightly built.${NC}"
+}
+
+# Build LAMP images.
+build_ubuntu-14.04-lamp-php5.6
+build_ubuntu-14.04-lamp-php7.0
+build_ubuntu-14.04-lamp-php7.1
+build_ubuntu-16.04-lamp-php7.0
+build_ubuntu-16.04-lamp-php7.1
+build_ubuntu-16.04-lamp-php7.2
+
+# Build .NET images.
+build_ubuntu-16.04-dotnet-sdk1.0.1
+build_ubuntu-16.04-dotnet-sdk2.0.2
---
+++ scripts/export_images.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+INFO='\033[0;34m'
+NC='\033[0m' # No Color
+
+WORKSPACE=/vagrant/workspace
+
+rm -f $WORKSPACE/*
+
+export_ubuntu-14.04-lamp-php5.6 () {
+	docker save proboci/ubuntu-14.04-lamp:php5.6-nightly > $WORKSPACE/ubuntu-14.04-php5.6-nightly.tar
+	gzip $WORKSPACE/ubuntu-14.04-php5.6-nightly.tar
+}
+
+export_ubuntu-14.04-lamp-php7.0 () {
+	docker save proboci/ubuntu-14.04-lamp:php7.0-nightly > $WORKSPACE/ubuntu-14.04-php7.0-nightly.tar
+	gzip $WORKSPACE/ubuntu-14.04-php7.0-nightly.tar
+}
+
+export_ubuntu-14.04-lamp-php7.1 () {
+	docker save proboci/ubuntu-14.04-lamp:php7.1-nightly > $WORKSPACE/ubuntu-14.04-php7.1-nightly.tar
+	gzip $WORKSPACE/ubuntu-14.04-php7.1-nightly.tar
+}
+
+export_ubuntu-14.04-lamp-php7.2 () {
+	docker save proboci/ubuntu-14.04-lamp:php7.2-nightly > $WORKSPACE/ubuntu-14.04-php7.2-nightly.tar
+	gzip $WORKSPACE/ubuntu-14.04-php7.2-nightly.tar
+}
+
+export_ubuntu-16.04-dotnet-sdk1.0.1 () {
+	docker save proboci/ubuntu-16.04-dotnet:sdk1.0.1-nightly > $WORKSPACE/ubuntu-16.04-dotnetsdk1.0.1-nightly.tar
+	gzip $WORKSPACE/ubuntu-16.04-dotnetsdk1.0.1-nightly.tar
+}
+
+export_ubuntu-16.04-dotnet-sdk2.0.2 () {
+	docker save proboci/ubuntu-16.04-dotnet:sdk2.0.2-nightly > $WORKSPACE/ubuntu-16.04-dotnetsdk2.0.2-nightly.tar
+	gzip $WORKSPACE/ubuntu-16.04-dotnetsdk2.0.2-nightly.tar
+}
+
+export_ubuntu-16.04-lamp-php7.0 () {
+	docker save proboci/ubuntu-16.04-lamp:php7.0-nightly > $WORKSPACE/ubuntu-16.04-php7.0-nightly.tar
+	gzip $WORKSPACE/ubuntu-16.04-php7.0-nightly.tar
+}
+
+export_ubuntu-16.04-lamp-php7.1 () {
+	docker save proboci/ubuntu-16.04-lamp:php7.1-nightly > $WORKSPACE/ubuntu-16.04-php7.1-nightly.tar
+	gzip $WORKSPACE/ubuntu-16.04-php7.1-nightly.tar
+}
+
+export_ubuntu-16.04-lamp-php7.2 () {
+	docker save proboci/ubuntu-16.04-lamp:php7.2-nightly > $WORKSPACE/ubuntu-16.04-php7.2-nightly.tar
+	gzip $WORKSPACE/ubuntu-16.04-php7.2-nightly.tar
+}
+
+# Build LAMP images.
+export_ubuntu-14.04-lamp-php5.6
+export_ubuntu-14.04-lamp-php7.0
+export_ubuntu-14.04-lamp-php7.1
+export_ubuntu-16.04-lamp-php7.0
+export_ubuntu-16.04-lamp-php7.1
+export_ubuntu-16.04-lamp-php7.2
+
+# Build .NET images.
+export_ubuntu-16.04-dotnet-sdk1.0.1
+export_ubuntu-16.04-dotnet-sdk2.0.2
