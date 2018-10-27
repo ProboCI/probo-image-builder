2ee90ebce74f6346a0d9cc5cbfb546d286e16b7e
Michael Bagnall <mrbagnall@icloud.com>
2018-10-27 12:28:27 -0400

    Changes for the mbagnall repository and postgres fixes.

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -17,97 +17,97 @@ BUILDER=/vagrant/builder
 build_ubuntu-14.04-lamp-php-5.5-beta () {
 	cd $BUILDER/14.04-php-5.5
 	$PACKER build ubuntu-14.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php-5.5-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-14.04-lamp:php-5.5-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php-5.5-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-14.04:php5.5-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-14.04:php5.5-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-14.04:php5.5-beta built.${NC}"
 }
 
 build_ubuntu-14.04-lamp-php-5.6-beta () {
 	cd $BUILDER/14.04-php-5.6
 	$PACKER build ubuntu-14.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php-5.6-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-14.04-lamp:php-5.6-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php-5.6-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-14.04:php5.6-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-14.04:php5.6-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-14.04:php5.6-beta built.${NC}"
 }
 
 build_ubuntu-14.04-lamp-php-7.0-beta () {
 	cd $BUILDER/14.04-php-7.0
 	$PACKER build ubuntu-14.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php-7.0-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-14.04-lamp:php-7.0-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php-7.0-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-14.04:php7.0-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-14.04:php7.0-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-14.04:php7.0-beta built.${NC}"
 }
 
 build_ubuntu-14.04-lamp-php-7.1-beta () {
 	cd $BUILDER/14.04-php-7.1
 	$PACKER build ubuntu-14.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php-7.1-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-14.04-lamp:php-7.1-beta" .
-	echo -e "${INFO}Image roboci/ubuntu-14.04-lamp:php-7.1-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-14.04:php7.1-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-14.04:php7.1-beta" .
+	echo -e "${INFO}Image roboci/ubuntu-14.04:php7.1-beta built.${NC}"
 }
 
 build_ubuntu-14.04-lamp-php-7.2-beta () {
 	cd $BUILDER/14.04-php-7.2
 	$PACKER build ubuntu-14.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:php-7.2-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-14.04-lamp:php-7.2-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:php-7.2-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-14.04:php7.2-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-14.04:php7.2-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-14.04:php7.2-beta built.${NC}"
 }
 
 build_ubuntu-14.04-lamp-latest () {
 	cd $BUILDER/14.04-latest
 	$PACKER build ubuntu-14.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-14.04-lamp:latest image...${NC}"
-	docker build --compress -t="proboci/ubuntu-14.04-lamp:latest" .
-	echo -e "${INFO}Image proboci/ubuntu-14.04-lamp:latest built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-14.04:latest image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-14.04:latest" .
+	echo -e "${INFO}Image mbagnall/ubuntu-14.04:latest built.${NC}"
 }
 
 build_ubuntu-16.04-lamp-php-7.0-beta () {
 	cd $BUILDER/16.04-php-7.0
 	$PACKER build ubuntu-16.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:php-7.0-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-16.04-lamp:php-7.0-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:php-7.0-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-16.04:php7.0-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-16.04:php7.0-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-16.04:php7.0-beta built.${NC}"
 }
 
 build_ubuntu-16.04-lamp-php-7.1-beta () {
 	cd $BUILDER/16.04-php-7.1
 	$PACKER build ubuntu-16.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:php-7.1-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-16.04-lamp:php-7.1-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:php-7.1-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-16.04:php7.1-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-16.04:php7.1-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-16.04:php7.1-beta built.${NC}"
 }
 
 build_ubuntu-16.04-lamp-php-7.2-beta () {
 	cd $BUILDER/16.04-php-7.2
 	$PACKER build ubuntu-16.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:php-7.2-beta image...${NC}"
-	docker build --compress -t="proboci/ubuntu-16.04-lamp:php-7.2-beta" .
-	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:php-7.2-beta built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-16.04:php7.2-beta image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-16.04:php7.2-beta" .
+	echo -e "${INFO}Image mbagnall/ubuntu-16.04:php7.2-beta built.${NC}"
 }
 
 build_ubuntu-16.04-lamp-latest () {
 	cd $BUILDER/16.04-latest
 	$PACKER build ubuntu-16.04-lamp.json
-	echo -e "${INFO}Building proboci/ubuntu-16.04-lamp:latest image...${NC}"
-	docker build --compress -t="proboci/ubuntu-16.04-lamp:latest" .
-	echo -e "${INFO}Image proboci/ubuntu-16.04-lamp:latest built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-16.04:latest image...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-16.04:latest" .
+	echo -e "${INFO}Image mbagnall/ubuntu-16.04:latest built.${NC}"
 }
 
 build_ubuntu-16.04-dotnet-sdk1.0.1 () {
 	cd $BUILDER/16.04-dotnetsdk1.0.1
 	$PACKER build ubuntu-16.04-dotnet.json
-	echo -e "${INFO}Building proboci/ubuntu-16.04-dotnet:sdk1.0.1...${NC}"
-	docker build --compress -t="proboci/ubuntu-16.04-dotnet:sdk1.0.1" .
-	echo -e "${INFO}Image proboci/ubuntu-16.04-dotnet:sdk1.0.1 built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-16.04-dotnet:sdk1.0.1...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-16.04-dotnet:sdk1.0.1" .
+	echo -e "${INFO}Image mbagnall/ubuntu-16.04-dotnet:sdk1.0.1 built.${NC}"
 }
 
 build_ubuntu-16.04-dotnet-sdk2.0.2 () {
 	cd $BUILDER/16.04-dotnetsdk2.0.2
 	$PACKER build ubuntu-16.04-dotnet.json
-	echo -e "${INFO}Building proboci/ubuntu-16.04-dotnet:sdk2.0.2...${NC}"
-	docker build --compress -t="proboci/ubuntu-16.04-dotnet:sdk2.0.2" .
-	echo -e "${INFO}Image proboci/ubuntu-16.04-dotnet:sdk2.0.2 built.${NC}"
+	echo -e "${INFO}Building mbagnall/ubuntu-16.04-dotnet:sdk2.0.2...${NC}"
+	docker build --compress -t="mbagnall/ubuntu-16.04-dotnet:sdk2.0.2" .
+	echo -e "${INFO}Image mbagnall/ubuntu-16.04-dotnet:sdk2.0.2 built.${NC}"
 }
 
 build_ubuntu-lamp-latest-images () {
